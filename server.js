c58e43d6dc4bfa57a4c105a0f3611817af6bd2e8
tianxiangbing <55342775@qq.com>
2017-06-29 11:00:13 +0800

    ip

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var config = require("./webpack.config.js");
 if(process.env.NODE_ENV =="dev"){
 config.entry.app.unshift("webpack-dev-server/client?http://localhost:9090/");
 }else{
-config.entry.app.unshift("webpack-dev-server/client?http://localhost:8080/");
+config.entry.app.unshift("webpack-dev-server/client?http://192.168.40.93:8080/");
 }
 var compiler = webpack(config);
 var server = new WebpackDevServer(compiler, {
@@ -13,5 +13,5 @@ var server = new WebpackDevServer(compiler, {
 if(process.env.NODE_ENV =="dev"){
 	server.listen(9090);
 }else{
-	server.listen(8080);
+	server.listen(8080,'192.168.40.93');
 }
